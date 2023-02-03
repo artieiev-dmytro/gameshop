701ab9f19589cfbaf0ec15fc70dc9a6ef8e8ee03
Dmytro Artieiev <dmytro.artieiv@gmail.com>
2023-02-03 17:38:48 +0200

    add view games

---
+++ games/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import games
+
+
+urlpatterns = [
+    path("", games, name="games"),
+]
--- games/views.py
+++ games/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "games/index.html")
+
+
+def games(request):
+    return render(request, "games/games.html")
