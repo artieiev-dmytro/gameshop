f93dfc1f0a734c8d4ad0a370632ea6297b40ff9c
Dmytro Artieiev <dmytro.artieiv@gmail.com>
2023-02-03 22:41:22 +0200

    add models

--- games/models.py
+++ games/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Developer(models.Model):
+    title = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    img = models.ImageField(upload_to="products_images", blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=True)
+    date = models.DateField()
+    view = models.PositiveIntegerField(default=0)
+    rating = models.IntegerField(default=0)
+    genge = models.ManyToManyField(Genre, on_delete=models.CASCADE)
+    developer = models.ForeignKey(Developer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
