e982fd5f8ffe501deedf98253c8e91d80122c0ab
Dmytro Artieiev <dmytro.artieiv@gmail.com>
2023-02-07 23:35:28 +0200

    add form UserLoginForm

--- users/views.py
+++ users/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login
+
+from .forms import UserLoginForm
 
 
 def login_view(request):
-    return render(request, "users/login.html")
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(reverse("games:games"))
+    else:
+        form = UserLoginForm()
+    context = {"form": form}
+    return render(request, "users/login.html", context)
 
 
 def register_view(request):
