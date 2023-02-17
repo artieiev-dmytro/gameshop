d973dc36fa52b1e3bfda22ee99ff0b6b6abccbfc
Dmytro Artieiev <dmytro.artieiv@gmail.com>
2023-02-17 19:38:10 +0200

    add postgresql

--- config/settings.py
+++ config/settings.py
@@ -53,10 +53,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "gameshop",
+        "USER": "gameshop_admin",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
