a04d07cbbc49a785b89502a03304905b7957c4dc
Dmytro Artieiev <dmytro.artieiv@gmail.com>
2023-02-18 19:44:14 +0200

    add first test

--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from django.urls import reverse
+from http import HTTPStatus
 
-# Create your tests here.
+from .models import Game, Developer, Genre
+
+
+class IndexViewTest(TestCase):
+    def test_view(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, "games/index.html")
+
+
+class GameViewTest(TestCase):
+    fixtures = ["game.json", "developer.json", "genre.json"]
+
+    def setUp(self):
+        self.games = Game.objects.all()
+
+    def test_list(self):
+        response = self.client.get(reverse("games:games"))
+        self._common_test(response)
+        self.assertEqual(
+            list(response.context_data["object_list"]), list(self.games[:1])
+        )
+
+    def test_list_with_developer(self):
+        developer = Developer.objects.first()
+        path = reverse("games:developer", kwargs={"developer_id": developer.id})
+        response = self.client.get(path)
+
+        self._common_test(response)
+        self.assertEqual(
+            list(response.context_data["object_list"]),
+            list(self.games.filter(developer=developer.id)),
+        )
+
+    def test_list_with_genre(self):
+        genre = Genre.objects.first()
+        path = reverse("games:genre", kwargs={"genre_id": genre.id})
+        response = self.client.get(path)
+
+        self._common_test(response)
+        self.assertEqual(
+            list(response.context_data["object_list"]),
+            [game for game in self.games if game.genre.filter(id=genre.id)][:1],
+        )
+
+    def _common_test(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, "games/games.html")
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from http import HTTPStatus
+from django.urls import reverse
 
-# Create your tests here.
+
+class UserRegistrationViewTest(TestCase):
+    def setUp(self):
+        self.path = reverse("users:register")
+
+    def test_user_ragistration_get(self):
+        response = self.client.get(self.path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, "users/register.html")
