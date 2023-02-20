14b77a17350cdd9538c530adc4e9838e7e0566eb
Dmytro Artieiev <dmytro.artieiv@gmail.com>
2023-02-21 00:52:38 +0200

    add CreateOrderView

--- orders/forms.py
+++ orders/forms.py
@@ -4,6 +4,27 @@ from .models import Order
 
 
 class OrderForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Enter first name"}
+        )
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Enter last name"}
+        )
+    )
+    email = forms.CharField(
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "Enter your email address"}
+        )
+    )
+    address = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Enter your address"}
+        )
+    )
+
     class Meta:
         model = Order
         fields = ("first_name", "last_name", "email", "address")
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 
@@ -11,6 +12,11 @@ from .cart import Cart
 class CreateOrderView(CreateView):
     template_name = "orders/create_order.html"
     form_class = OrderForm
+    success_url = reverse_lazy("orders:create_order")
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super().form_valid(form)
 
 
 class CartView(TemplateView):
